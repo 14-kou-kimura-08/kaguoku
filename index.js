ff1ae95e7fa479f7814362d04537f23aad01faad
kimura_kouichirou <814.kimura86@gmail.com>
2020-05-08 08:54:23 +0900

    Introduction to swiper

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import Moveable from "moveable";
+import Swiper from "swiper";
 
 let moveable;
 
@@ -69,3 +70,45 @@ window.addEventListener("keydown", function(event) {
     moveable = new Moveable(document.body)
   }
 }, true);
+
+
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    },
+
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  })
